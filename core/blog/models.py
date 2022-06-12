f451b9dda6f54b7acf1168eef62a52a4d30eeb68
Armin Darabi Mahboub <armindarabimahboub@gmail.com>
2022-06-12 14:43:05 +0430

    post and category models created

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    image = models.ImageField(null=True, blank=True)
+    content = models.TextField()
+    status = models.BooleanField()
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
+    
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
