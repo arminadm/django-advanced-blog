215e1ced7cf62ab1d350d63b9fb582fbfc91daf1
Armin Darabi Mahboub <armindarabimahboub@gmail.com>
2022-06-12 19:54:45 +0430

    details of Post and Category added to admin panel

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from blog.models import Post, Category 
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['author', 'title', 'image', 'status', 'category', 'created_date', 'updated_date', 'published_date']
+    list_filter = ['status', 'category']
+    search_fields = ['author', 'title', 'category']    
+    ordering = ['-created_date']
+admin.site.register(Post, PostAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_date', 'updated_date']
+    search_fields = ['name']
+    ordering = ['-created_date']
+admin.site.register(Category, CategoryAdmin)
