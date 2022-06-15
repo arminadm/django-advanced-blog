f1d11447494c7f01df2fd5b63b6b45b357a45f55
Armin Darabi Mahboub <armindarabimahboub@gmail.com>
2022-06-15 12:59:14 +0430

    changed PostSerializer super class to ModelSerializer + post method added to api_post_list function so user can make new posts by rest

--- core/blog/api/v1/serializer.py
+++ core/blog/api/v1/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
+from ...models import Post
 
-class PostSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=250)
-    
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'author', 'title', 'content', 'status', 'category', 'created_date', 'updated_date', 'published_date']
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -4,15 +4,26 @@ from rest_framework.response import Response
 from ...models import Post
 from .serializer import PostSerializer
 
-@api_view()
+@api_view(['GET', 'POST'])
 def api_post_list(request):
-    post = Post.objects.filter(status=True)
-    serializedPost = PostSerializer(post, many=True)
-    return Response(serializedPost.data)
+    if request.method == 'GET':
+        post = Post.objects.filter(status=True)
+        serializedPost = PostSerializer(post, many=True)
+        return Response(serializedPost.data)
+    elif request.method == 'POST':
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
 
 @api_view()
 def api_post_detail(request, pk):
-    post = get_object_or_404(Post, id=pk)
-    serializedPost = PostSerializer(post)
-    return Response(serializedPost.data)
+    if request.method == 'GET':
+        post = get_object_or_404(Post, id=pk, status=True)
+        serializedPost = PostSerializer(post)
+        return Response(serializedPost.data)
+        
+
+
 
