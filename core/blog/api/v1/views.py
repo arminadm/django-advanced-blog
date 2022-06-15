f7b7e39198810a0c194dfd4fc31c1d8eaf40d5e4
Armin Darabi Mahboub <armindarabimahboub@gmail.com>
2022-06-15 13:12:15 +0430

    Put method added to api_post_detail so user can change the post's details

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -8,8 +8,8 @@ from .serializer import PostSerializer
 def api_post_list(request):
     if request.method == 'GET':
         post = Post.objects.filter(status=True)
-        serializedPost = PostSerializer(post, many=True)
-        return Response(serializedPost.data)
+        serializer = PostSerializer(post, many=True)
+        return Response(serializer.data)
     elif request.method == 'POST':
         serializer = PostSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -17,13 +17,15 @@ def api_post_list(request):
         return Response(serializer.data)
     
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 def api_post_detail(request, pk):
     if request.method == 'GET':
         post = get_object_or_404(Post, id=pk, status=True)
-        serializedPost = PostSerializer(post)
-        return Response(serializedPost.data)
-        
-
-
-
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        post = get_object_or_404(Post, id=pk, status=True)
+        serializer = PostSerializer(post, request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(request.data)
