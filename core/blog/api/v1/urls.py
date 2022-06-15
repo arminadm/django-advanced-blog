d115137652f993bf07853513cc8f1c96c579fba9
Armin Darabi Mahboub <armindarabimahboub@gmail.com>
2022-06-15 21:36:53 +0430

    changed our api-v1 views from function base to class base

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-from .views import api_post_detail, api_post_list
+from .views import PostList, PostDetail
 
 app_name = 'api-v1'
 
 urlpatterns = [
-    path('posts/', api_post_list, name='api-post-list'),
-    path('posts/<int:pk>/', api_post_detail, name='api-post-detail')
+    # path('posts/', api_post_list, name='api-post-list'),
+    # path('posts/<int:pk>/', api_post_detail, name='api-post-detail')
+    path('posts/', PostList.as_view(), name='api-post-list'),
+    path('posts/<int:pk>/', PostDetail.as_view(), name='api-post-detail'),
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,11 +1,14 @@
+# from django.shortcuts import get_object_or_404
+# from rest_framework.decorators import api_view, permission_classes
 from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from ...models import Post
 from .serializer import PostSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+from rest_framework.views import APIView
 
 
+"""
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticatedOrReadOnly])
 def api_post_list(request):
@@ -37,3 +40,35 @@ def api_post_detail(request, pk):
         post = get_object_or_404(Post, id=pk, status=True)
         post.delete()
         return Response({"detail": "post removed successfully"})
+"""
+
+class PostList(APIView):
+    def get(self, request):
+        posts = Post.objects.filter(status=True)
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+class PostDetail(APIView):
+    def get(self, request, pk):
+        post = get_object_or_404(Post, status=True, id=pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        post = get_object_or_404(Post, status=True, id=pk)
+        serializer = PostSerializer(post, request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    def delete(self, request, pk):
+        post = get_object_or_404(Post, status=True, id=pk)
+        post.delete()
+        return Response({"detail":"Post object removed successfully"})
