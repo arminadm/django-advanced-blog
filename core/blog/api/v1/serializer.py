9ba8f35b10bf94f16d94a0dc11af7607db82ae6c
Armin Darabi Mahboub <armindarabimahboub@gmail.com>
2022-06-17 14:20:30 +0430

    created snippet and relative_url(which is not working good enough, will get better in the next commit)
    created two new methods in Post models for snippet and relative_url

--- core/blog/api/v1/serializer.py
+++ core/blog/api/v1/serializer.py
@@ -2,11 +2,13 @@ from rest_framework import serializers
 from ...models import Post, Category
 
 class PostSerializer(serializers.ModelSerializer):
-    content = serializers.ReadOnlyField() #first way to make specific filed to readonly
+    # content = serializers.ReadOnlyField() #first way to make specific filed to readonly
     # content = serializers.CharField(read_only=True) # second way to make specific filed to readonly
+    snippet = serializers.ReadOnlyField(source='get_snippet')
+    relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True)
     class Meta:
         model = Post
-        fields = ['id', 'author', 'title', 'content', 'status', 'category', 'created_date', 'updated_date', 'published_date']
+        fields = ['id', 'author', 'title', 'content', 'snippet', 'status', 'category', 'relative_url', 'created_date', 'updated_date', 'published_date']
         # read_only_fields = ['content'] # third way to make specific filed to readonly
 
 class CategorySerializer(serializers.ModelSerializer):
--- core/blog/models.py
+++ core/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
+from django.urls import reverse
 # getting user model object
 User = get_user_model()
 
@@ -16,6 +16,12 @@ class Post(models.Model):
     updated_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField()
     
+    def get_snippet(self):
+        return self.content[0:8]
+    
+    def get_absolute_api_url(self): #changed the name from get_absolute_url to get_absolute_api_url so we dont get any conflict (it can be named anything)
+        return reverse("blog:api-v1:post-detail", kwargs={"pk": self.pk})
+
     def __str__(self):
         return self.title
 
