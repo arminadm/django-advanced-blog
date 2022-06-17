892ac90407e9cba4f62f0cc126769e42f353595e
Armin Darabi Mahboub <armindarabimahboub@gmail.com>
2022-06-17 14:39:38 +0430

    added absolute_url func to post serializer so we can get better Post's urls

--- core/blog/api/v1/serializer.py
+++ core/blog/api/v1/serializer.py
@@ -6,10 +6,18 @@ class PostSerializer(serializers.ModelSerializer):
     # content = serializers.CharField(read_only=True) # second way to make specific filed to readonly
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True)
+    absolute_url = serializers.SerializerMethodField(method_name='get_absolute_url') #the name default of function has to be get_ + name of variable, but if you wanted to name function
+    #anything else, you can use method_name to solve your problem
+    
     class Meta:
         model = Post
-        fields = ['id', 'author', 'title', 'content', 'snippet', 'status', 'category', 'relative_url', 'created_date', 'updated_date', 'published_date']
+        fields = ['id', 'author', 'title', 'content', 'snippet', 'status', 'category', 'relative_url', 'absolute_url', 'created_date', 'updated_date', 'published_date']
         # read_only_fields = ['content'] # third way to make specific filed to readonly
+
+    def get_absolute_url(self, obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
