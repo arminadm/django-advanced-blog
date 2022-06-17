50f8c6d6022b339454fa7fab03b169d727062890
Armin Darabi Mahboub <armindarabimahboub@gmail.com>
2022-06-17 14:05:17 +0430

    adjust 3 ways to set specific field to readonly

--- core/blog/api/v1/serializer.py
+++ core/blog/api/v1/serializer.py
@@ -2,9 +2,12 @@ from rest_framework import serializers
 from ...models import Post, Category
 
 class PostSerializer(serializers.ModelSerializer):
+    content = serializers.ReadOnlyField() #first way to make specific filed to readonly
+    # content = serializers.CharField(read_only=True) # second way to make specific filed to readonly
     class Meta:
         model = Post
         fields = ['id', 'author', 'title', 'content', 'status', 'category', 'created_date', 'updated_date', 'published_date']
+        # read_only_fields = ['content'] # third way to make specific filed to readonly
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
