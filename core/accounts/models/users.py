e33229aed434568bd34c55df9581ec895828dbd2
Armin Darabi Mahboub <armindarabimahboub@gmail.com>
2022-06-23 15:47:17 +0430

    account models.py removed and transferred into new models folder
    for having better clean code, we separated models into users.py and profile.py

--- core/accounts/models/users.py
+++ core/accounts/models/users.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
-from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
-from django.db.models.signals import post_save
 
 # Create your models here.
 class CustomeUserManager(BaseUserManager):
@@ -60,21 +58,3 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
-
-class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    image = models.ImageField(null=True, blank=True)
-    description = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.user.email
-
-
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
