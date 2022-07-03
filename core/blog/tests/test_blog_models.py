2472b020398e8bdfb6043e3111df543ba8233e66
Armin Darabi Mahboub <armindarabimahboub@gmail.com>
2022-07-03 19:43:07 +0430

    added setup config to TestPostModel

--- core/blog/tests/test_blog_models.py
+++ core/blog/tests/test_blog_models.py
@@ -4,20 +4,24 @@ from datetime import datetime
 from accounts.models import User, Profile
 
 class TestPostModel(TestCase):
-    def test_create_new_post_model(self):
-        user = User.objects.create(email = 'test@testcase.com')
-        profile = Profile.objects.create(
-            user = user,
+    def setUp(self):
+        self.user = User.objects.create(email = 'test@testcase.com')
+        self.profile = Profile.objects.create(
+            user = self.user,
             first_name = 'testcase_first_name',
             last_name = 'testcase_last_name',
         )
-        category = Category.objects.create(name='test_category')
+        self.category = Category.objects.create(name='test_category')
+
+    def test_create_new_post_model_with_valid_data(self):
         post = Post.objects.create(
-            author = profile,
+            author = self.profile,
             title = 'this post created by test case',
             content = 'this is a content about that post which created by test case',
             status = True,
-            category = category,
+            category = self.category,
             published_date = datetime.now()
         )
+        self.assertTrue(Post.objects.filter(pk=post.id).exists())
         self.assertEquals(post.title, 'this post created by test case')
+    
