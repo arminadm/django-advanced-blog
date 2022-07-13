73e146a99cd90a43c88c7bd29888f0264e902614
arminadm <armindarabimahboub@gmail.com>
2022-07-13 21:24:59 +0430

    used cache_page to cache data for faster server reply

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,4 +1,6 @@
 from django.http import JsonResponse, HttpResponse
+from django.core.cache import cache
+from django.views.decorators.cache import cache_page
 from time import sleep
 from accounts.tasks import send_email
 from requests import get
@@ -7,8 +9,10 @@ from requests import get
 def sendEmail(request):
     # sleep(3)
     send_email.delay()
-    return HttpResponse('<h1>Email send successfully</h1>')
+    return HttpResponse("<h1>Email send successfully</h1>")
 
+# cache info for 15 seconds
+@cache_page(15)
 def testing_cache_time_delay(request):
-    response = get('https://65cbad3d-952d-4f60-91f9-4f9a722f56ce.mock.pstmn.io/test/delay_5s')
+    response = get("https://65cbad3d-952d-4f60-91f9-4f9a722f56ce.mock.pstmn.io/test/delay_5s")    
     return JsonResponse(response.json())
--- core/core/settings.py
+++ core/core/settings.py
@@ -220,7 +220,8 @@ CELERY_BROKER_URL = 'redis://redis:6379/1'
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://redis:6379/2",
+        # "TIMEOUT": 3,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
