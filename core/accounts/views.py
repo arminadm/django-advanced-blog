aea7821ee85cc0abad92bb79de8d310347a8b93e
arminadm <armindarabimahboub@gmail.com>
2022-07-13 21:02:52 +0430

    created mock server to test time delay and installed django-redis

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,9 +1,14 @@
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponse
 from time import sleep
 from accounts.tasks import send_email
+from requests import get
 
 # Create your views here.
 def sendEmail(request):
     # sleep(3)
     send_email.delay()
     return HttpResponse('<h1>Email send successfully</h1>')
+
+def testing_cache_time_delay(request):
+    response = get('https://65cbad3d-952d-4f60-91f9-4f9a722f56ce.mock.pstmn.io/test/delay_5s')
+    return JsonResponse(response.json())
--- core/core/settings.py
+++ core/core/settings.py
@@ -214,3 +214,15 @@ CELERY_BROKER_URL = 'redis://redis:6379/1'
 #         "schedule":5,
 #     },
 # }
+
+
+# django-redis configs
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
